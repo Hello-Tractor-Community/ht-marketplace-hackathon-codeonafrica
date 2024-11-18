7c9dcaea89149d59562430a12d8cbec320de4957
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-11-18 22:04:51 -0100

    navbar

--- Client/src/Components/NavBar.jsx
+++ Client/src/Components/NavBar.jsx
@@ -1,12 +1,28 @@
 import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
 
 
 
 const NavBar = () => {
   return (
-    <div>
+    <nav className='bg-white text-black flex items-center justify-between py-4 px-16'>
         <img className='h-20 ' src="logo.svg" alt="logo" />
-    </div>
+
+        <div className=' w-1/3 flex items-center justify-around font-semibold'>
+         <Link to ="/tractors" className=''> Tractors </Link >
+         <Link to ="/equipments"> Equipments </Link >
+         <Link to ="/about"> About </Link >
+         <Link to ="/contact us"> Contact Us </Link >
+        
+        
+            
+
+        </div>
+        <div className='space-x-4 '>
+            <button className=' bg-black text-white px-8 py-2 rounded-md font-semibold'> Sign up</button>
+            <button className='text-black border-black bg-white border-2 px-8 py-2 rounded-md font-semibold'>Login</button>
+        </div>
+    </nav>
   )
 }
 
